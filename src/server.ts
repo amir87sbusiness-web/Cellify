9f20784aa9ae63ef4ceaf263158b7a1650c7a640
amir87sbusiness-web <amir87.s.business@gmail.com>
2026-05-31 12:21:45 +0330

    Remove all files properly

--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import { createRequestHandler } from "@tanstack/start/server";
-import { defaultHandler } from "@tanstack/start/server";
-
-// ایجاد هندلر استاندارد و بومی سرور بدون وابستگی به ابزارهای نظارتی Lovable
-export default createRequestHandler({
-  requestHandler: defaultHandler,
-});
